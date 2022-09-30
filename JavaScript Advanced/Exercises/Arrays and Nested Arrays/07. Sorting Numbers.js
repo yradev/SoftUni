0e3992558c2a437232b6657ce8795f7d1a5b7699
yradev <85577992+yradev@users.noreply.github.com>
2022-09-30 23:04:50 +0300

    update

--- JavaScript Advanced/Exercises/Arrays and Nested Arrays/07. Sorting Numbers.js
+++ JavaScript Advanced/Exercises/Arrays and Nested Arrays/07. Sorting Numbers.js
@@ -1,13 +1,36 @@
-function solve(array){
-    const finalArray = [];
-    array.sort((a,b)=>a-b);
+// function solve(array){
+//     const finalArray = [];
+//     array.sort((a,b)=>a-b);
 
-    while(array.length>0){
-        finalArray.push(array.shift());
-        finalArray.push(array.pop())
+//     while(array.length>0){
+//         finalArray.push(array.shift());
+//         finalArray.push(array.pop())
+//     }
+
+//     return finalArray;
+// };
+
+function echo(array) {
+    let result = [];
+    array.sort(function(a, b){return a - b});
+    let biggest = 0;
+    let smallest = 0;
+    let counter = 0;
+     
+     
+    for(let el of array){
+    result.push(el);
+    // counter=0;
+    while(counter === 0){
+        smallest = array.pop();
+        result.push(smallest);
+        // counter=1;
+        break;
+    }
+     
+    }
+     
+    return result;
     }
 
-    return finalArray;
-};
-
-solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
+console.log(echo([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
